950bed9e50ab883cecf1590195f7a893177c40ed
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-06 10:34:25 +0000

    creating an AboutActivity, which can be launched from the settings page

---
+++ src/com/mwr/droidhg/agent/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.mwr.droidhg.agent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class AboutActivity extends Activity {
+	
+	private TextView description;
+	
+	private String getVersionName() {
+		try {
+			return this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+		}
+		catch(NameNotFoundException e) {
+			return "ERR";
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		
+		this.description = (TextView)this.findViewById(R.id.about_mercury_description);
+		this.description.setText(String.format(this.getString(R.string.about_mercury_description), this.getVersionName()));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return false;
+	}
+
+}
