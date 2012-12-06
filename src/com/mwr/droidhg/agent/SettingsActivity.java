01fdece9deba17c33f57917b2da65aa0e34404e2
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-06 10:33:18 +0000

    add a 'System' group to the settings screen, and an 'About Mercury' item under it

--- src/com/mwr/droidhg/agent/SettingsActivity.java
+++ src/com/mwr/droidhg/agent/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.mwr.droidhg.agent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
+	
+	private Preference about_preference = null;
 
 	@Override
 	@SuppressWarnings("deprecation")
@@ -11,6 +15,18 @@ public class SettingsActivity extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 		
 		this.addPreferencesFromResource(R.xml.preferences);
+		
+		this.about_preference = (Preference)this.findPreference("about_mercury");
+		this.about_preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+			@Override
+			public boolean onPreferenceClick(Preference arg0) {
+				SettingsActivity.this.startActivity(new Intent(SettingsActivity.this, AboutActivity.class));
+				
+				return true;
+			}
+			
+		});
 	}
 	
 }
