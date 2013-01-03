cddaaf529444cb4a5dbd0dea939d7ca6e07f9bb4
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-01-03 14:46:02 +0000

    adding a load of comments to the Connection class

--- src/com/mwr/droidhg/connector/Connection.java
+++ src/com/mwr/droidhg/connector/Connection.java
@@ -13,6 +13,12 @@ import com.mwr.droidhg.api.builders.SystemRequestFactory;
 import com.mwr.droidhg.api.Handler;
 import com.mwr.droidhg.api.SystemMessageHandler;
 
+/**
+ * A Connection is created by a Connector when a live transport connection is available.
+ * 
+ * The Connection polls the connector for new Messages, and routes them to the appropriate
+ * handler.
+ */
 public class Connection extends Thread {
 	
 	private static final long LIVENESS_THRESHOLD = 30000L; // milliseconds
@@ -29,6 +35,12 @@ public class Connection extends Thread {
 		this.transport = transport;
 	}
 	
+	/**
+	 * Attempt to handshake with a Server to bind this device, sharing the device id, manufacturer,
+	 * model and software version.
+	 * 
+	 * Note: this is only used if we are operating as a Client (see {@link #mustBind()}).
+	 */
 	private boolean bindToServer() {
 		if(this.mustBind()) {
 			this.send(new MessageFactory(SystemRequestFactory.bind().setDevice()).setId(1).build());
@@ -55,6 +67,12 @@ public class Connection extends Thread {
 			return true;
 	}
 	
+	/**
+	 * Perform a liveness check. If we haven't received a message in the last {@link #LIVENESS_THRESHOLD} milliseconds,
+	 * then we stop the connection.
+	 * 
+	 * Note: this is only used if {@link Connector#checkForLiveness()} is true.
+	 */
 	private void checkForLiveness() {
 		if(this.connector.checkForLiveness() && System.currentTimeMillis() - this.last_message_at > LIVENESS_THRESHOLD || !this.transport.isLive()) {
 			Log.i("connection", "connection was reset, no message for " + (System.currentTimeMillis() - this.last_message_at) + "ms");
@@ -63,10 +81,24 @@ public class Connection extends Thread {
 		}
 	}
 	
+	/**
+	 * Gets all Sessions associated with this Connection.
+	 */
 	public Collection<Session> getSessions() {
 		return this.connector.getSessions();
 	}
 	
+	/**
+	 * handleMessage is invoked by the main {@link #run()} loop, each time a Message has
+	 * been received from the peer. It inspects the message, and dispatches it to the
+	 * appropriate handler:
+	 * 
+	 *   - all SYSTEM_REQUEST messages are dispatched to {@link #system_message_handler};
+	 *   - all REFLECTION_REQUEST messages are dispatched to the appropriate Session;
+	 *   - other messages are discarded.
+	 * 
+	 * If the handler returns a response message, it is delivered to the peer automatically.
+	 */
 	private void handleMessage(Message message) {
 		try {
 			Message response = null;
@@ -104,14 +136,29 @@ public class Connection extends Thread {
 		this.last_message_at = System.currentTimeMillis();
 	}
 	
+	/**
+	 * @return true, if there are sessions associated with this Connection
+	 */
 	public boolean hasSessions() {
 		return this.connector.hasSessions();
 	}
 	
+	/**
+	 * @return true, if the connection must bind to its peer
+	 */
 	private boolean mustBind() {
 		return this.connector.mustBind();
 	}
 	
+	/**
+	 * Attempt to receive a Message from the peer.
+	 * 
+	 * If a full Message is available, it is removed from its headers and returned as a
+	 * Message object. If no message (or a partial message) is available, null is returned.
+	 * 
+	 * If something has gone wrong (i.e., an invalid API version, or dropped connection) the
+	 * Connection will be stopped, and null is returned.
+	 */
 	public Message receive() {
 		try {
 			Frame f = this.transport.receive();
@@ -147,6 +194,19 @@ public class Connection extends Thread {
 		}
 	}
 	
+	/**
+	 * Executed by the Java runtime when the Connection thread starts.
+	 * 
+	 * This manages the entire lifecycle of a connection:
+	 * 
+	 *   1. Bind to the peer, if necessary.
+	 *   2. Receive a message from the peer.
+	 *   3. Interpret and act on the message.
+	 *      This may send a result.
+	 *   4. Perform liveness checks.
+	 *   5. Iff the connection is still alive, return to (2).
+	 *   6. Unbind from the peer, if necessary.
+	 */
 	public void run() {
 		this.running = true;
 		this.started = true;
@@ -175,6 +235,9 @@ public class Connection extends Thread {
 		this.unbindFromServer();
 	}
 	
+	/**
+	 * Deliver a message to the peer.
+	 */
 	public void send(Message message) {
 		try {
 			this.transport.send(new Frame(message));
@@ -187,14 +250,29 @@ public class Connection extends Thread {
 		}
 	}
 	
+	/**
+	 * Start a new Mercury Session.
+	 * 
+	 * This is invoked by the {@link #system_message_handler}, iff it receives a
+	 * START_SESSION message.
+	 * 
+	 * We defer to the {@link #connector}, which owns the Session, and return the resultant
+	 * handle.
+	 */
 	public Session startSession() {
 		return this.connector.startSession();
 	}
 	
+	/**
+	 * Stops the connection, forcing all active sessions to die.
+	 */
 	public void stopConnection() {
 		this.stopConnection(true);
 	}
 	
+	/**
+	 * Ask the connection to stop, optionally killing all active sessions.
+	 */
 	public void stopConnection(boolean kill_sessions) {
 		this.running = false;
 		
@@ -202,14 +280,29 @@ public class Connection extends Thread {
 			this.stopSessions();
 	}
 	
+	/**
+	 * Stop a Mercury Session.
+	 * 
+	 * This is invoked by the {@link #system_message_handler}, iff it receives a
+	 * STOP_SESSION message.
+	 * 
+	 * We defer to the {@link #connector}, which owns the Session.
+	 */
 	public Session stopSession(String session_id) {
 		return this.connector.stopSession(session_id);
 	}
 	
+	/**
+	 * Stops all sessions associated with this connection.
+	 */
 	public void stopSessions() {
 		this.connector.stopSessions();
 	}
 	
+	/**
+	 * Attempt to disconnect from the server, indicating that our device id is not longer
+	 * available.
+	 */
 	private void unbindFromServer() {
 		if(this.mustBind()) {
 			this.send(new MessageFactory(SystemRequestFactory.unbind().setDevice()).setId(1).build());
