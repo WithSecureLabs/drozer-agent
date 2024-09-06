1ba47e65c43b353fe2c086e92225e940d6ea35e4
Nathaniel Smith <nathaniel.ollie.smith@gmail.com>
2024-09-06 03:26:19 +0100

    reindented GlobalSettings.java with tabs

--- agent/src/main/java/com/WithSecure/dz/models/GlobalSettings.java
+++ agent/src/main/java/com/WithSecure/dz/models/GlobalSettings.java
@@ -12,74 +12,75 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class GlobalSettings {
-    private GlobalSettings(Context ctx) {
-        // load settings from config file
-        try {
-            BufferedReader confFile = new BufferedReader(new InputStreamReader(
-                    ctx.getResources().openRawResource(R.raw.config)));
-            String line;
+	private GlobalSettings(Context ctx) {
+		// load settings from config file
+		try {
+			BufferedReader confFile = new BufferedReader(new InputStreamReader(
+					ctx.getResources().openRawResource(R.raw.config)));
+			String line;
 
-            while ((line = confFile.readLine()) != null) {
-                String[] parts = line.split(":", 2);
+			while ((line = confFile.readLine()) != null) {
+				String[] parts = line.split(":", 2);
 
-                if (parts.length != 2) {
-                    continue;
-                }
+				if (parts.length != 2) {
+					continue;
+				}
 
-                data.put(parts[0], parts[1]);
+				data.put(parts[0], parts[1]);
 
-                // preform additional changes on a per setting basis
-                switch (parts[0]) {
-                    case "server-port":
-                        try {
-                            SharedPreferences.Editor e = Agent.getInstance().getSettings().edit();
-                            e.putString(Server.SERVER_PORT, parts[1]);
-                            e.commit();
-                            } catch (NumberFormatException ignored) {
-                                break;
-                            }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        catch (Resources.NotFoundException ignored) { }
-        catch (IOException ignored) { }
-    }
+				// preform additional changes on a per setting basis
+				switch (parts[0]) {
+					case "server-port":
+						try {
+							SharedPreferences.Editor e = Agent.getInstance().getSettings().edit();
+							e.putString(Server.SERVER_PORT, parts[1]);
+							e.commit();
+						}
+						catch (NumberFormatException ignored) {
+							break;
+						}
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		catch (Resources.NotFoundException ignored) { }
+		catch (IOException ignored) { }
+	}
 
-    public static String get(String key) {
-        return get(key, null);
-    }
+	public static String get(String key) {
+		return get(key, null);
+	}
 
-    public static String get(String key, String fallback) {
-        String value = instance.data.get(key);
+	public static String get(String key, String fallback) {
+		String value = instance.data.get(key);
 
-        return value != null
-                ? value
-                : fallback;
-    }
+		return value != null
+				? value
+				: fallback;
+	}
 
-    public static int themeFromString(String s) {
-        if (s == null) {
-            return R.style.AppTheme;
-        }
+	public static int themeFromString(String s) {
+		if (s == null) {
+			return R.style.AppTheme;
+		}
 
-        switch(s) {
-            case "red":		return R.style.dzRed;
-            case "green":	return R.style.dzGreen;
-            case "purple":	return R.style.dzPurple;
-            case "blue":	return R.style.dzBlue;
-            default:		return R.style.AppTheme;
-        }
-    }
+		switch(s) {
+			case "red":		return R.style.dzRed;
+			case "green":	return R.style.dzGreen;
+			case "purple":	return R.style.dzPurple;
+			case "blue":	return R.style.dzBlue;
+			default:		return R.style.AppTheme;
+		}
+	}
 
-    public static void Init(Context ctx) {
-        if (instance == null) {
-            instance = new GlobalSettings(ctx);
-        }
-    }
+	public static void Init(Context ctx) {
+		if (instance == null) {
+			instance = new GlobalSettings(ctx);
+		}
+	}
 
-    private static GlobalSettings instance = null;
-    private final Dictionary<String, String> data = new Hashtable<>();
+	private static GlobalSettings instance = null;
+	private final Dictionary<String, String> data = new Hashtable<>();
 }
